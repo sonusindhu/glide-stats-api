18ed626563a75ca46358279467284a38404dbdbf
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-07-09 19:17:28 +0530

    updated user migration

--- database/migrations/1657373137635_users.ts
+++ database/migrations/1657373137635_users.ts
@@ -6,7 +6,17 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('type', 255).notNullable()
       table.string('email', 255).notNullable()
+      table.string('firstName', 255).notNullable()
+      table.string('lastName', 255).notNullable()
+      table.string('fullName', 255).notNullable()
+      table.string('jobName', 255).nullable()
+      table.string('location', 255).nullable()
+      table.string('department', 255).nullable()
+      table.string('phoneNumber', 50).nullable()
+      table.string('extension', 10).nullable()
+      table.string('image', 255).nullable()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
