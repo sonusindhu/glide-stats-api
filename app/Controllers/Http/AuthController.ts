e6ef2e756e8efb8c8d5169dcdda23f23fbd12a8c
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-07-13 20:07:30 +0530

    updated auth register

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -14,10 +14,23 @@ export default class AuthController {
     const email = request.input('email')
     const password = request.input('password')
     const firstName = request.input('firstName')
+    const lastName = request.input('lastName')
+    const jobTitle = request.input('jobTitle')
+    const department = request.input('department')
+    const phoneNumber = request.input('phoneNumber')
+    const extension = request.input('extension')
+    const location = request.input('location')
     const newUser = new User()
     newUser.email = email
     newUser.password = password
     newUser.firstName = firstName
+    newUser.lastName = lastName
+    newUser.lastName = `${firstName} ${lastName}`
+    newUser.jobTitle = jobTitle
+    newUser.department = department
+    newUser.phoneNumber = phoneNumber
+    newUser.extension = extension
+    newUser.location = location
     await newUser.save()
     const token = await auth.use('api').login(newUser, {
       expiresIn: '10 days',
