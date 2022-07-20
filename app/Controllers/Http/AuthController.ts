46b68be794cd52225adf811395cceece49c0a01d
Sonu Sindhu <sonusindhu10@yahoo.com>
2022-07-20 21:25:35 +0530

    added admin

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -24,6 +24,32 @@ export default class AuthController {
     const firstName = request.input('firstName')
     const lastName = request.input('lastName')
     const newUser = new User()
+    newUser.type = 4
+    newUser.email = request.input('email')
+    newUser.password = request.input('password')
+    newUser.firstName = request.input('firstName')
+    newUser.lastName = request.input('lastName')
+    newUser.lastName = `${firstName} ${lastName}`
+    newUser.jobTitle = request.input('jobTitle')
+    newUser.department = request.input('department')
+    newUser.phoneNumber = request.input('phoneNumber')
+    newUser.extension = request.input('extension')
+    newUser.location = request.input('location')
+    newUser.status = 1
+    await newUser.save()
+    return newUser
+    // const token = await auth.use('api').login(newUser, {
+    //   expiresIn: '10 days',
+    // })
+    // return token.toJSON();
+  }
+
+  public async addAdmin({ request, response }: HttpContextContract) {
+    // console.log(request)
+    // return request.body()
+    const firstName = request.input('firstName')
+    const lastName = request.input('lastName')
+    const newUser = new User()
     newUser.type = 1
     newUser.email = request.input('email')
     newUser.password = request.input('password')
